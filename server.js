5281e9f33084ab333044c2a4a82d973e56122024
LINDA HINTZE <lindahintze@hotmail.com>
2021-01-18 14:53:21 +0100

    created database + add json data to database

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+import clinicData from './data/clinic-data.json'
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/healthFinder"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// HERE GOES THE MODELS
+
+const Clinic = new mongoose.model('Clinic', {
+  region: String,
+  clinic_operation: String,
+  clinic_type: String,
+  clinic_name: String,
+  address: String,
+  open_hours: String
+})
+
+// Use RESET_DATABASE = true in Heruko to reset
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database!')
+  const seedDatabase = async () => {
+    await Clinic.deleteMany({}) 
+    clinicData.forEach(async clinicData => {
+      await new Clinic(clinicData).save()
+    })
+  }
+  seedDatabase()
+}
+
+
+// Defines the port the app will run on
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -13,10 +41,17 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// HERE GOES THE ROUTES
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('This is my final project API')
 })
+
+// GET requests to display clinic data
+
+// POST request to perform reviews 
+
+// GET requests to display reviews 
 
 // Start the server
 app.listen(port, () => {
